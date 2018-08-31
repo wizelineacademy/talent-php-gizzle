d8bae62998bd1d2a04d09525e05742b58404a57c
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2018-08-31 01:01:30 -0500

    Implement Magic methods

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $event = new \App\Event('PHP Advanced (CMS) Certification');
-$event->setDate('2018-08-30');
+$event->date = '2018-08-30';
 
-echo "Event: $event->title, When: " . $event->getDate();
+echo "Event: $event->title, When: $event->date";
--- src/Event.php
+++ src/Event.php
@@ -16,14 +16,34 @@ class Event {
     /**
      * @param string $date Y-m-d
      */
-    function setDate($date) {
+    protected function setDate($date) {
         $this->date = Carbon::createFromFormat('Y-m-d', $date);
     }
 
     /**
      * @return string Y-m-d
      */
-     function getDate() {
+    protected function getDate() {
         return $this->date->format('Y-m-d');
+    }
+
+    public function __set($key, $value) {
+        $function = camel_case('set_' . $key);
+        
+        if (!method_exists($this, $function)) {
+            throw new \ErrorException("Undefined Property `$key`");
+        }
+
+        $this->$function($value);
+    }
+
+    public function __get($key) {
+        $function = camel_case('get_' . $key);
+
+        if (!method_exists($this, $function)) {
+            throw new \ErrorException("Undefined Property `$key`");
+        }
+
+        return $this->$function();
     }
 }
